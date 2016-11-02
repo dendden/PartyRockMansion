552c4fd6ec769f4794fb2f7b8f516cb758fdf6d9
Denys Triasunov <dendden@users.noreply.github.com>
2016-11-02 10:33:06 +0200

    main view and video playing view

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Денис Трясунов on 11/1/16.
+//  Copyright © 2016 Денис Трясунов. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
+
